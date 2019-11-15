a473e3933f31dcce7f70b529936e909b8c0a521b
Anton Kechashin <atk@sitecore.net>
2019-11-15 12:48:27 +0200

    Fix for 9.0.2

---
+++ src/Sitecore.Support.373914/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.373914")]
+[assembly: AssemblyProduct("Sitecore.Support.373914")]
+[assembly: ComVisible(false)]
